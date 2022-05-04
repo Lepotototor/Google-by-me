c47dba7b1c13855a3d27146456782804f2a52cac
Victør Flament <victor.flament@protonmail.com>
2022-05-04 21:39:48 +0200

    I don't think it's work for smartphones

--- src/script.js
+++ src/script.js
@@ -11,11 +11,13 @@ barre.onkeydown = function (event) {
 }
 
 function doSearch() {
-  let debut = "https://www.google.com/search?q=";
-  let milieu = "&sxsrf=ALiCzsZZ_RvSYctFYnGOxHYSBykT6MlWSw%3A1651670232165&source=hp&ei=2HxyYufJB9KSa5W-lpAL&iflsig=AJiK0e8AAAAAYnKK6B_hRrs4jDMOXJQpjxv4s55Pmh5G&ved=0ahUKEwjnleOL98X3AhVSyRoKHRWfBbIQ4dUDCAc&uact=5&oq=";
-  let fin = "&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBOgcIIxDqAhAnOgQIIxAnOgQIABBDOgUIABCABDoLCC4QgAQQxwEQ0QM6CwguEIAEEMcBEKMCOggILhCABBDUAjoOCC4QgAQQxwEQ0QMQ1AI6BQguEIAEOgcIABCABBAKOgsIABCABBAKEAEQKjoJCAAQgAQQChABOgQIABAKOgQILhAKOggIABAFEAoQHjoECAAQDToGCAAQDRAKOgQILhANOg4ILhCABBDHARCjAhDUAlDtAljZ3UBgxd5AaANwAHgBgAHBAYgB3xWSAQQ3LjE2mAEAoAEBsAEK&sclient=gws-wiz";
-  let research = barre.value.replace(/\s+/g, '+');
-  let url = debut + research + milieu + research + fin;
-  document.location.href = url;
-  console.log(url);
+  if (barre.value != '') {
+    let debut = "https://www.google.com/search?q=";
+    let milieu = "&sxsrf=ALiCzsZZ_RvSYctFYnGOxHYSBykT6MlWSw%3A1651670232165&source=hp&ei=2HxyYufJB9KSa5W-lpAL&iflsig=AJiK0e8AAAAAYnKK6B_hRrs4jDMOXJQpjxv4s55Pmh5G&ved=0ahUKEwjnleOL98X3AhVSyRoKHRWfBbIQ4dUDCAc&uact=5&oq=";
+    let fin = "&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBOgcIIxDqAhAnOgQIIxAnOgQIABBDOgUIABCABDoLCC4QgAQQxwEQ0QM6CwguEIAEEMcBEKMCOggILhCABBDUAjoOCC4QgAQQxwEQ0QMQ1AI6BQguEIAEOgcIABCABBAKOgsIABCABBAKEAEQKjoJCAAQgAQQChABOgQIABAKOgQILhAKOggIABAFEAoQHjoECAAQDToGCAAQDRAKOgQILhANOg4ILhCABBDHARCjAhDUAlDtAljZ3UBgxd5AaANwAHgBgAHBAYgB3xWSAQQ3LjE2mAEAoAEBsAEK&sclient=gws-wiz";
+    let research = barre.value.replace(/\s+/g, '+');
+    let url = debut + research + milieu + research + fin;
+    document.location.href = url;
+    console.log(url);
+  }
 }
